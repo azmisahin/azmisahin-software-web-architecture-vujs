2191873c6a3b7352ab28530818076f973c6429c9
Azmi ŞAHİN <azmisahin@outlook.com>
2018-11-23 22:12:50 +0300

    Add a Vue Dependecies

---
+++ src/components/welcome.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="welcome">
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+	name: "welcome",
+	props: {
+		title: String,
+		message: String
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+	color: #42b983;
+}
+</style>
