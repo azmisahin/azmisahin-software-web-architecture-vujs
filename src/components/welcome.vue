b40fa11d8160825f71fe6aec9c0d11b9728d8765
Azmi ŞAHİN <azmisahin@outlook.com>
2018-12-08 12:37:06 +0300

    Sassy CSS Added

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -18,9 +18,11 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1 {
-  color: #42b983;
-  }
-  </style>
+<style lang="scss" scoped>
+@import "./src/styles/variable.scss";
+
+// Welcome Component
+h1{
+  color           : $secondary-color;
+}
+</style>
